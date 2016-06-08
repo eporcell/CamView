4b19fe206523f11b9f8aaebfb1a3f8825bac7b96
Edgardo <e_porcell@hotmail.com>
2016-06-07 20:16:10 -0400

    Several updates...

--- CamView.py
+++ CamView.py
@@ -2,6 +2,7 @@ import os.path                      # libraries for pathnames
 import cv2                          # libraries for camera and processing
 from PyQt4 import QtGui, QtCore     # libraries for GUI implementation
 import sys                          # system specific functions
+from PIL import Image               # for opening images
 
 # libraries for image processing
 import matplotlib
@@ -14,6 +15,15 @@ class Capture():
         self.capturing = False
         self.c = cv2.VideoCapture(0)
 
+        self.pixel_threshold = 80           # default value for pixel threshold 80
+        self.pixel_max_size = 255           # default value for max pixel size 255
+        self.kernel_size = (7,7)            # default value for Kernel Size
+        self.std_Deviation = 1              # default value for Std Deviation
+        self.h_threshold1 = 90              # default value for Hysteresis Threshold 1
+        self.h_threshold2 = 110             # default value for Hysteresis Threshold 2
+
+        self.show_canny_image = True        # automatically show processed canny image by default
+
         home_dir = os.path.expanduser("~")                  # get home directory
 
         # define all directory locations
@@ -22,9 +32,9 @@ class Capture():
         c_dir = home_dir + "/Pictures/CamView/processed"    # processed image directory
 
         # define all file names
-        self.image_dir = i_dir + "/img_{}.png"              # captured images from Cam
-        self.canny_dir = c_dir + "/canny_{}.png"            # images processed by canny edge detection
-        self.index_dir = x_dir + "/.index"                  # index file
+        self.index_dir = x_dir + "/.index"                          # index file
+        self.image_dir = i_dir + "/img_{}.png"                      # captured images from Cam
+        self.canny_dir = c_dir + "/canny_{}_{}:{}-{}:{}-{}:{}.png"  # images processed by canny edge detection
 
         # make sure a folder exists to save captured images
         if not os.path.exists(i_dir):                       # check if folder exists
@@ -53,13 +63,13 @@ class Capture():
         # Starts image capturing
         # This function is called by the 'Start' button
     def startCapture(self):
-        print ("pressed start")
+        print ("pressed Start")
         self.capturing = True
         cap = self.c
         while(self.capturing):
             ret, frame = cap.read()
-            cv2.imshow("Capture", frame)            # show capture window
-            cv2.moveWindow("Capture", 1275, 0)      # move capture window to desired location
+            cv2.imshow("Capture", frame)                    # show capture window
+            cv2.moveWindow("Capture", 1275, 0)              # move window to desired location
             cv2.waitKey(5)
         cv2.destroyAllWindows()
 
@@ -104,38 +114,77 @@ class Capture():
         self.img_index = 0                                  # resets index variable to 0
         print("WARNING: Image index reset!")                # warns about index being reset
 
+    def cannyCheckBox(self):                                # Toogle value of show_canny_image
+        self.show_canny_image = not self.show_canny_image   # Called by the 'Open Image' checkbox
+
+    def pixelThreshold(self, val):                          # update value from pixel threshold spinbox
+        self.pixel_threshold = val                          # pass value to pixel threshold variable
+
+    def pixelMaxSize(self, val):                            # update value from kernel size spinbox
+        self.pixel_max_size = val                           # pass value to pixel max size variable
+
+    def kernelSize(self, val):                              # update value from kernel size spinbox
+        self.kernel_size = (val,val)                        # pass value to Kernel size variable
+
+    def stdDeviation(self, val):                            # update value from Standard Deviation spinbox
+        self.std_Deviation = val                            # pass value to Std Deviation variable
+
+    def hysteresisThreshold_1(self, val):                   # update value from hysteresisThreshold_1 spinbox
+        self.h_threshold1 = val                             # pass value to Threshold 1 variable
+
+    def hysteresisThreshold_2(self, val):                   # update value from hysteresisThreshold_2 spinbox
+        self.h_threshold2 = val                             # pass value to Threshold 2 variable
+
         # Perform Canny Edge detection on latest image captured
         # This function is called by the 'Canny Edges' button
     def cannyEdges(self):
 
         img_name = self.image_dir.format(self.img_index)    # name of latest image captured
 
-        # make sure the captured image exists
-        if os.path.exists(img_name):            # check if an image exists
+        if os.path.exists(img_name):                        # check if an image exists
 
-            canny = cv2.imread(img_name)        # read latest image captured
+            k_size = self.kernel_size
+            std_dev = self.std_Deviation
+
+            canny = cv2.imread(img_name)                    # read latest image captured
+            blur = cv2.GaussianBlur(canny, k_size, std_dev) # perform gaussian blur
+
+            p_thr = self.pixel_threshold
+            p_max = self.pixel_max_size
 
             # Preprocess Image
-            canny_pre  = cv2.cvtColor(cv2.GaussianBlur(canny, (7,7), 0), cv2.COLOR_BGR2GRAY)    # preprocess by blurring and grayscale
-            _, canny_thresh = cv2.threshold(canny_pre, 80, 255, cv2.THRESH_BINARY)              # find binary image with thresholding
-            plt.imshow(cv2.cvtColor(canny_thresh, cv2.COLOR_GRAY2RGB))
+            blur_grey  = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)           # grayscale blurred image
+            _, canny_thr = cv2.threshold(blur_grey, p_thr, p_max, cv2.THRESH_BINARY)  # find binary image with thresholding
+            plt.imshow(cv2.cvtColor(canny_thr, cv2.COLOR_GRAY2RGB))
+
+            thr_1 = self.h_threshold1
+            thr_2 = self.h_threshold2
+
+            # Determine the name of canny image to be processed
+            k_size_str = str(k_size).replace(" ", "")       # remove blank spaces in k_size
+            canny_name = self.canny_dir.format(self.img_index, p_thr, p_max, k_size_str, std_dev, thr_1, thr_2)
 
             # Canny Edge detection
-            canny_edges = cv2.Canny(canny_pre, threshold1=90, threshold2=110)
-            plt.imshow(cv2.cvtColor(canny_edges, cv2.COLOR_GRAY2RGB))
-            cv2.imwrite(self.canny_dir.format(self.img_index), canny_edges)     # writes image on the folder
-            print("Canny Edge detection image saved as {}".format(img_name))    # send message to terminal
+            edges = cv2.Canny(blur_grey, thr_1, thr_2)
+            plt.imshow(cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))
+            cv2.imwrite(canny_name, edges)                                  # writes image on the folder
+            print("Canny Edge detection image saved as " + canny_name)      # send message to terminal
 
-        else:                                       # if the image does not exists
-            print("WARNING: Image not present.")    # show message in terminal
-            print("Please capture an image")
+            if self.show_canny_image:                       # if Checkbox is checked
+                img = Image.open(canny_name)                # open processed image
+                img.show()                                  # and display it
+            else:
+                pass
+
+        else:                                               # if the image does not exists
+            print("WARNING: No image available.")           # show message in terminal
 
 class Window(QtGui.QMainWindow):
 
 	# Initializations
 	def __init__(self):
 		super(Window, self).__init__()
-		self.setGeometry(50, 50, 500, 300)
+		self.setGeometry(50, 50, 550, 300)
 		self.setWindowTitle("CamView")
 		self.setWindowIcon(QtGui.QIcon("./icons/opencv_logo.png"))
 
@@ -150,6 +199,17 @@ class Window(QtGui.QMainWindow):
 		fileMenu = mainMenu.addMenu('&File')
 		fileMenu.addAction(extractAction)
 
+		# This is a stub. Need to complete the GroupBox for the 'Capture' section
+		TracParamFrame = QtGui.QGroupBox(self)
+		TracParamFrame.setTitle("Capture")
+		TracParamFrame.move(50, 35)
+
+		# This is a stub. Need to complete the GroupBox for the 'Canny Edge' section
+		TracParamFrame = QtGui.QGroupBox(self)
+		TracParamFrame.setTitle("Canny Edge Detection")
+		TracParamFrame.resize(200,50)
+		TracParamFrame.move(275, 35)
+
 		self.home()
 
 	# Views
@@ -159,32 +219,112 @@ class Window(QtGui.QMainWindow):
 		self.capture = Capture()
 		btn = QtGui.QPushButton("Start", self)
 		btn.clicked.connect(self.capture.startCapture)
-		btn.resize(55,50)                                # set button size. Use 'btn.resize(btn.sizeHint())' for minimum size
-		btn.move(25,50)
+		btn.resize(55,50)                                # Use 'btn.resize(btn.sizeHint())' for minimum size
+		btn.move(25,75)
 
 		# Stop capture button
 		btn = QtGui.QPushButton("Stop", self)
 		btn.clicked.connect(self.capture.endCapture)
 		btn.resize(55,50)
-		btn.move(85,50)
+		btn.move(85,75)
 
 		# Save Picture button
 		btn = QtGui.QPushButton("Save Picture", self)
 		btn.clicked.connect(self.capture.savePicture)
 		btn.resize(115,50)
-		btn.move(25,125)
+		btn.move(25,140)
 
-        # Reset Index button
+		# Reset Index button
 		btn = QtGui.QPushButton("Reset Index", self)
 		btn.clicked.connect(self.capture.resetIndex)
 		btn.resize(115,50)
-		btn.move(25,200)
+		btn.move(25,205)
 
-        # Canny Edge button
-		btn = QtGui.QPushButton("Canny Edges", self)
+		# Vertical divider for the Capture and Processing sections
+		#div = QtGui.QFrame(self)
+		#div.setFrameStyle(self.VLine)
+		#div.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Expanding)
+
+		# Canny Edge button
+		btn = QtGui.QPushButton("Detect Edges", self)
 		btn.clicked.connect(self.capture.cannyEdges)
+		btn.move(410,75)
 		btn.resize(115,50)
-		btn.move(200,50)
+
+		# Checkbox to automatically open canny image after processing
+		cbx = QtGui.QCheckBox("Open Image", self)
+		cbx.move(410, 125)
+		cbx.resize(115,40)
+		cbx.toggle()
+		cbx.stateChanged.connect(self.capture.cannyCheckBox)
+
+		klab = QtGui.QLabel("Pixel Threshold:", self)
+		klab.move(180, 65)
+		klab.setFixedWidth(120)
+
+		klab = QtGui.QLabel("Pixel Max Size:", self)
+		klab.move(190, 95)
+		klab.setFixedWidth(120)
+
+		klab = QtGui.QLabel("Kernel Size:", self)
+		klab.move(210, 135)
+
+		klab = QtGui.QLabel("Std Deviation:", self)
+		klab.move(195, 165)
+
+		tlab = QtGui.QLabel("Threshold 1:", self)
+		tlab.move(205, 205)
+
+		tlab = QtGui.QLabel("Threshold 2:", self)
+		tlab.move(205, 235)
+
+		# Spinbox for Pixel Threshold
+		spn1 = QtGui.QSpinBox(self)
+		spn1.move(315, 65)
+		spn1.setFixedWidth(70)
+		spn1.setRange(0,500)
+		spn1.setValue(80)
+		spn1.valueChanged.connect(self.capture.pixelThreshold)
+
+		# Spinbox for Pixel Maximim Size
+		spn1 = QtGui.QSpinBox(self)
+		spn1.move(315, 95)
+		spn1.setFixedWidth(70)
+		spn1.setRange(0,500)
+		spn1.setValue(255)
+		spn1.valueChanged.connect(self.capture.pixelMaxSize)
+
+		# Spinbox for Kernel Size for Gaussian filter
+		spn1 = QtGui.QSpinBox(self)
+		spn1.move(315, 135)
+		spn1.setFixedWidth(70)
+		spn1.setRange(0,500)
+		spn1.setValue(7)
+		spn1.valueChanged.connect(self.capture.kernelSize)
+
+		# Spinbox for Standard Deviation for Gaussian filter
+		spn2 = QtGui.QSpinBox(self)
+		spn2.move(315, 165)
+		spn2.setFixedWidth(70)
+		spn2.setRange(0,500)
+		spn2.setValue(1)
+		spn2.valueChanged.connect(self.capture.stdDeviation)
+
+		# Spinbox for Hysteresis Threshold 1 values
+		spn3 = QtGui.QSpinBox(self)
+		spn3.move(315, 205)
+		spn3.setFixedWidth(70)
+		spn3.setRange(0,500)
+		spn3.setValue(90)
+		spn3.valueChanged.connect(self.capture.hysteresisThreshold_1)
+
+		# Spinbox for Hysteresis Threshold 2 values
+		spn4 = QtGui.QSpinBox(self)
+		spn4.move(315, 235)
+		spn4.setFixedWidth(70)
+		spn4.setRange(0,500)
+		spn4.setValue(110)
+		spn4.valueChanged.connect(self.capture.hysteresisThreshold_2)
 
 		self.show()
 
--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # CamView
 
+	This software provides a simple gui to capture images on a webcam using PyQt4 and OpenCV3.
+
+	It also enables the user to use 'Canny Edge' detection on the images captured.
+
+	All captured and processed images are automatically saved to separate directories on ~/Pictures/CamView. The directories will be created if they do not exist. It is assumed the software will be run on a Linux system.
+
+# Capture Section
+
+	The capture section is design to work similar to a point-and-shoot camera:
+
+	By pressing the 'Save' button, it saves the captured image to the designated folder. An image index is appended to the end of each of the new images saved.
+
+# Canny Edge Detection Section
+
+	I'll update this as I have time...
+
+For future use:
+
 In order to set up the development environment on a new system, it is 
 recommended to follow these steps:
 
@@ -14,3 +32,5 @@ recommended to follow these steps:
 
 4) Install git-cola or other git client. To install git-cola from the terminal:
 	sudo apt-get install git-cola
+
+Send any questions to eporcell@protonmail.ch
